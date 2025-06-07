efc3143043b5f5f948537a8c23fdade5d229c444
James Coates <james.coates@me.com>
2025-06-07 09:58:10 +0200

    setup prettier

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,15 +1,15 @@
-import { TestComponent } from '@repo/ui/test'
-import { createFileRoute } from '@tanstack/react-router'
+import { TestComponent } from '@repo/ui/test';
+import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-    component: Index,
-})
+  component: Index,
+});
 
 function Index() {
-    return (
-        <div className="p-2 p-12">
-            <h3>Welcome Home!</h3>
-            <TestComponent />
-        </div>
-    )
+  return (
+    <div className="p-2 p-12">
+      <h3>Welcome Home!</h3>
+      <TestComponent />
+    </div>
+  );
 }
